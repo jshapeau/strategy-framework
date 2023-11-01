e9b7b0dd5188c8d6ffd6450758d4edc3be81fba1
= <jshapkin+github@gmail.com>
2023-11-01 13:05:15 -0600

    Update Command for ECS

--- ActionSystem/Command/Command.cs
+++ ActionSystem/Command/Command.cs
@@ -3,41 +3,34 @@ using System;
 public class GridCellCommand : ICommand
 {
     public string Name { get; private set; }
-    public Action<IGameAction> OnExecute { get; set; }
+    public Action<ActionData> OnExecute { get; set; }
     public IDirectionalSelector Selector { get => this.selector; }
 
-
-    private IUnit Unit { get; } //ToDo: Should be provided directly to the action by factory method.
-    private Func<GridCellCollection> getGridCells;
-    private IGameAction GameAction;
+    private GridObjectEntity source { get; }
+    private MovementGameAction GameAction;
     private IGridCellSelector selector;
 
-    // ToDo: getGridCells should be part of Selector, and supplied vie a factory method.
     public GridCellCommand(
         string name,
-        IUnit unit,
-        IGridCellSelector selector,
-        Func<GridCellCollection> getGridCells)
+        GridObjectEntity source,
+        IGridCellSelector selector)
     {
         this.Name = name;
-        this.Unit = unit;
+        this.source = source;
         this.selector = selector;
-        this.getGridCells = getGridCells;
     }
 
     public void Initialize()
     {
-        this.selector.Initialize(this.getGridCells());
+        this.selector.Initialize(this.source.Location);
         this.selector.OnSelect += Execute;
     }
 
     public void Execute(GridCellCollection cells)
     {
-        GridPositionCollection positions = cells.GetGridPositions();
-
-        //ToDo: GameAction Should be provided by a factory method once GameAction is fully implemented.
-        this.GameAction = new MoveGameAction(positions, Unit.GridObject);
-        OnExecute?.Invoke(GameAction);
+        this.GameAction = new MovementGameAction(this.source);
+        GameAction.Execute(cells);
+        this.OnExecute.Invoke(GameAction.data);
     }
 
     public override string ToString()
